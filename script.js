0a771549f3af45a7591753532a3efa796c98cf72
rcarlos-silva <48447957+rcarlos-silva@users.noreply.github.com>
2020-05-27 15:22:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var count=20;
 function start(){
     if ((count - 1) >=0 ){
         count=count - 1;
-        tempo.innerText=`A PROMOÇÃO VAI ACABAR EM ${count} MINUTOS`;
+        tempo.innerText=`A PROMOÇÃO VAI ACABAR EM ${count} SEGUNDOS`;
         setTimeout('start();', 1000);
        
     }
